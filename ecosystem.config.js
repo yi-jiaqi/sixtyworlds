4c9a21cf503ff8633747a636b75e4ca655a74023
Jiaqi Yi <jy4421@nyu.edu>
2025-04-12 16:53:38 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@ module.exports = {
   apps: [{
     name: 'sw',
     script: 'app.js',
-    env_production: {
+    env_pro: {
       NODE_ENV: 'pro',
       PORT: 3001
     },
 	//for production
-    env_development: {
+    env_dev: {
       NODE_ENV: 'dev',
       PORT: 3001
     }
